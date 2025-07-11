a17f4d65ef8443c601c51481d9c4bdf48c4ca33c
Paula Boutala <paulamaciel.3@gmail.com>
2025-07-10 23:18:00 -0300

    Initial commit
    Project setup: basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-export default function App() {
+import imgGasPump from "./assets/logo.png";
+
+function App() {
   return (
     <div>
-      <h1>Meu projeto</h1>
+      <main className="container">
+        <img className="imagem" src={imgGasPump} alt="bomba de gasolina" />
+        <h1 className="title">Qual a melhor opção?</h1>
+        <form className="form">
+          <label>Álcool (preço por litro):</label>
+          <input
+            className="input"
+            type="number"
+            placeholder="Insira o valor"
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <label>Gasolina (preço por litro):</label>
+          <input
+            className="input"
+            type="number"
+            placeholder="Insira o valor"
+            min="1"
+            step="0.01"
+            required
+          />
+          <input className="button" type="submit" value="Calcular" />
+        </form>
+      </main>
     </div>
   );
 }
+
+export default App;
