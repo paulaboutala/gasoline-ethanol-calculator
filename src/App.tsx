c92063f53d8d6277ef6f11af656e41a82e01fee3
Paula Boutala <paulamaciel.3@gmail.com>
2025-07-27 20:28:19 -0300

    App.tsx
    Function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import "./app.css";
 import imgGasPump from "./assets/logo.png";
+import { useState } from "react";
 
 function App() {
+  const [gasolinaInput, setGasolinaInput] = useState(1);
+  const [alcoolInput, setAlcoolInput] = useState(1);
+
+  function calcular(event) {
+    alert("Teste!");
+  }
+
   return (
     <div>
       <main className="container">
         <img className="imagem" src={imgGasPump} alt="bomba de gasolina" />
         <h1 className="title">Qual a melhor opção?</h1>
-        <form className="form">
+        <form className="form" onSubmit={calcular}>
           <label>Álcool (preço por litro):</label>
           <input
             className="input"
